eb177b4cd7900cf0c507598ddf327538c60db6e4
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2020-02-15 14:15:12 -0600

    Reindent project file. Add quil

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject generative-art-live "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license
+  {:name "Eclipse Public License"
+   :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring-server "0.5.0"]
@@ -12,25 +13,34 @@
                  [ring/ring-defaults "0.3.2"]
                  [hiccup "1.0.5"]
                  [yogthos/config "1.1.7"]
-                 [org.clojure/clojurescript "1.10.597"
-                  :scope "provided"]
+                 [org.clojure/clojurescript
+                  "1.10.597"
+                  :scope
+                  "provided"]
                  [metosin/reitit "0.3.10"]
                  [pez/clerk "1.0.0"]
-                 [venantius/accountant "0.2.5"
-                  :exclusions [org.clojure/tools.reader]]]
+                 [quil "3.1.0"]
+                 [venantius/accountant
+                  "0.2.5"
+                  :exclusions
+                  [org.clojure/tools.reader]]]
 
-  :plugins [[lein-environ "1.1.0"]
-            [lein-cljsbuild "1.1.7"]
-            [lein-asset-minifier "0.4.6"
-             :exclusions [org.clojure/clojure]]]
+  :plugins
+  [[lein-environ "1.1.0"]
+   [lein-cljsbuild "1.1.7"]
+   [lein-asset-minifier "0.4.6"
+    :exclusions
+    [org.clojure/clojure]]]
 
-  :ring {:handler generative-art-live.handler/app
-         :uberwar-name "generative-art-live.war"}
+  :ring
+  {:handler      generative-art-live.handler/app
+   :uberwar-name "generative-art-live.war"}
 
   :min-lein-version "2.5.0"
   :uberjar-name "generative-art-live.jar"
   :main generative-art-live.server
-  :clean-targets ^{:protect false}
+  :clean-targets
+  ^{:protect false}
   [:target-path
    [:cljsbuild :builds :app :compiler :output-dir]
    [:cljsbuild :builds :app :compiler :output-to]]
@@ -39,71 +49,64 @@
   :resource-paths ["resources" "target/cljsbuild"]
 
   :minify-assets
-  [[:css {:source "resources/public/css/site.css"
-          :target "resources/public/css/site.min.css"}]]
+  [[:css
+    {:source "resources/public/css/site.css"
+     :target "resources/public/css/site.min.css"}]]
 
   :cljsbuild
   {:builds {:min
             {:source-paths ["src/cljs" "src/cljc" "env/prod/cljs"]
              :compiler
-             {:output-to        "target/cljsbuild/public/js/app.js"
-              :output-dir       "target/cljsbuild/public/js"
-              :source-map       "target/cljsbuild/public/js/app.js.map"
+             {:output-to     "target/cljsbuild/public/js/app.js"
+              :output-dir    "target/cljsbuild/public/js"
+              :source-map    "target/cljsbuild/public/js/app.js.map"
               :optimizations :advanced
               :infer-externs true
               :pretty-print  false}}
             :app
             {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-             :figwheel {:on-jsload "generative-art-live.core/mount-root"}
+             :figwheel     {:on-jsload "generative-art-live.core/mount-root"}
              :compiler
-             {:main "generative-art-live.dev"
-              :asset-path "/js/out"
-              :output-to "target/cljsbuild/public/js/app.js"
-              :output-dir "target/cljsbuild/public/js/out"
-              :source-map true
+             {:main          "generative-art-live.dev"
+              :asset-path    "/js/out"
+              :output-to     "target/cljsbuild/public/js/app.js"
+              :output-dir    "target/cljsbuild/public/js/out"
+              :source-map    true
               :optimizations :none
-              :pretty-print  true}}
-
-
-
-            }
-   }
+              :pretty-print  true}}}}
 
   :figwheel
   {:http-server-root "public"
-   :server-port 3449
-   :nrepl-port 7002
-   :nrepl-middleware [cider.piggieback/wrap-cljs-repl
-                      ]
-   :css-dirs ["resources/public/css"]
-   :ring-handler generative-art-live.handler/app}
+   :server-port      3449
+   :nrepl-port       7002
+   :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
+   :css-dirs         ["resources/public/css"]
+   :ring-handler     generative-art-live.handler/app}
 
 
+  :profiles
+  {:dev     {:repl-options {:init-ns generative-art-live.repl}
+             :dependencies [[cider/piggieback "0.4.2"]
+                            [binaryage/devtools "0.9.11"]
+                            [ring/ring-mock "0.4.0"]
+                            [ring/ring-devel "1.8.0"]
+                            [prone "2019-07-08"]
+                            [figwheel-sidecar "0.5.19"]
+                            [nrepl "0.6.0"]
+                            [pjstadig/humane-test-output "0.10.0"]]
 
-  :profiles {:dev {:repl-options {:init-ns generative-art-live.repl}
-                   :dependencies [[cider/piggieback "0.4.2"]
-                                  [binaryage/devtools "0.9.11"]
-                                  [ring/ring-mock "0.4.0"]
-                                  [ring/ring-devel "1.8.0"]
-                                  [prone "2019-07-08"]
-                                  [figwheel-sidecar "0.5.19"]
-                                  [nrepl "0.6.0"]
-                                  [pjstadig/humane-test-output "0.10.0"]
-                                  
- ]
+             :source-paths ["env/dev/clj"]
+             :plugins      [[lein-figwheel "0.5.19"]]
 
-                   :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.19"]
-]
+             :injections   [(require
+                             'pjstadig.humane-test-output)
+                            (pjstadig.humane-test-output/activate!)]
 
-                   :injections [(require 'pjstadig.humane-test-output)
-                                (pjstadig.humane-test-output/activate!)]
+             :env          {:dev true}}
 
-                   :env {:dev true}}
-
-             :uberjar {:hooks [minify-assets.plugin/hooks]
-                       :source-paths ["env/prod/clj"]
-                       :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
-                       :env {:production true}
-                       :aot :all
-                       :omit-source true}})
+   :uberjar {:hooks        [minify-assets.plugin/hooks]
+             :source-paths ["env/prod/clj"]
+             :prep-tasks   ["compile" ["cljsbuild" "once" "min"]]
+             :env          {:production true}
+             :aot          :all
+             :omit-source  true}})
